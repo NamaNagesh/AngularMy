cf4a1fd33c59fb12b32d5959dc136a39fd4fd641
NamaNagesh <53150226+NamaNagesh@users.noreply.github.com>
2022-08-29 23:25:34 -0700

    add gojs

--- src/app/product-template-details/product-template-details.component.ts
+++ src/app/product-template-details/product-template-details.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import {ActivatedRoute} from '@angular/router';
+import * as go from 'gojs';
+
+const $= go.GraphObject.make;
 
 @Component({
   selector: 'app-product-template-details',
@@ -10,11 +13,39 @@ export class ProductTemplateDetailsComponent implements OnInit {
 
   constructor(private route: ActivatedRoute) { }
   templateId:any;
+  public diagram:any=null;
   ngOnInit(): void {
     this.route.params.subscribe(params=>{
       console.log("id",params['id'])
       this.templateId=params['id'];
     })
   }
+  ngAfterViewInit(){
+    this.diagram = $(go.Diagram,'MyDia')
+    this.diagram.nodeTemplate= $(go.Node,"Auto",
+    $(go.Shape,"RoundedRectangle",
+      {fill:"cyan",portId:"",fromLinkable:true,toLinkable:true},
 
+     ),
+     $(go.TextBlock,{margin:8},
+       new go.Binding("text","key")
+      ),
+    )
+    this.diagram.linkTemplate=$(go.Link,{relinkableFrom:true,relinkableTo:true},
+        $(go.Shape),
+        $(go.Shape,{toArrow:"Standard"})
+      )
+
+   this.diagram.model=$(go.GraphLinksModel,{
+     nodeDataArray:[
+       {key:"Router"},
+       {key:"Transformer"}
+     ],
+     linkDataArray:
+     [
+       {from:"Router",to:"Transformer"}
+     ]
+   })   
+  }
+  
 }
